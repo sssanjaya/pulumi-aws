ba5dcd0c6b31eb4e9253b59f6ea9694012cbfe64
sssanjaya <sanjay.hona@gmail.com>
2023-05-22 10:28:50 -0400

    new default approach

--- aws/index.ts
+++ aws/index.ts
@@ -3,10 +3,10 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import { getStack } from "@pulumi/pulumi";
 
-// const config = new pulumi.Config();
-// const containerPort = config.getNumber("containerPort") || 80;
-// const cpu = config.getNumber("cpu") || 512;
-// const memory = config.getNumber("memory") || 128;
+const config = new pulumi.Config();
+const containerPort = config.getNumber("containerPort") || 80;
+const cpu = config.getNumber("cpu") || 512;
+const memory = config.getNumber("memory") || 128;
 
 //A common tags to reuse 
 const commonTags = {
@@ -19,9 +19,6 @@ const commonTags = {
 // set up VPC
 const vpc = new awsx.ec2.Vpc("vpc",{
     numberOfAvailabilityZones: 3,
-    natGateways: {
-        strategy: awsx.ec2.NatGatewayStrategy.Single,
-    }
 })
 
 // An ECS cluster to deploy into
@@ -29,64 +26,9 @@ const cluster = new aws.ecs.Cluster("cluster", {
     tags: commonTags,
 });
 
-// Security Group
-const group = new aws.ec2.SecurityGroup("web-secgrp", {
-    vpcId: vpc.vpcId,
-    description: "Enable HTTP access",
-    ingress: [{
-      protocol: "tcp",
-      fromPort: 80,
-      toPort: 80,
-      cidrBlocks: ["0.0.0.0/0"],
-    }],
-    egress: [{
-      protocol: "-1",
-      fromPort: 0,
-      toPort: 0,
-      cidrBlocks: ["0.0.0.0/0"],
-    }],
-});
-
-// Add load balancer
-const loadbalancer = new aws.lb.LoadBalancer("loadbalancer", {
-    securityGroups: [group.id],
-    subnets: vpc.publicSubnetIds,
+// An ALB to serve the container endpoint to the internet
+const loadbalancer = new awsx.lb.ApplicationLoadBalancer("loadbalancer", {
     tags: commonTags,
-});
-
-const targetGroup = new aws.lb.TargetGroup("app-tg", {
-    port: 80,
-    protocol: "HTTP",
-    targetType: "ip",
-    vpcId: vpc.vpcId,
-  });
-
-const listener = new aws.lb.Listener("web", {
-  loadBalancerArn: loadbalancer.arn,
-  port: 80,
-  defaultActions: [{
-    type: "forward",
-    targetGroupArn: targetGroup.arn,
-  }],
-});  
-
-const role = new aws.iam.Role("task-exec-role", {
-    assumeRolePolicy: JSON.stringify({
-        Version: "2008-10-17",
-        Statement: [{
-        Action: "sts:AssumeRole",
-        Principal: {
-            Service: "ecs-tasks.amazonaws.com",
-        },
-        Effect: "Allow",
-        Sid: "",
-        }],
-    }),
-});
-  
-new aws.iam.RolePolicyAttachment("task-exec-policy", {
-    role: role.name,
-    policyArn: aws.iam.ManagedPolicy.AmazonECSTaskExecutionRolePolicy,
 });
 
 // An ECR repository to store our application's container image
@@ -113,113 +55,42 @@ const api = new awsx.ecr.Image("api", {
 });
 
 // Deploy an ECS Service on Fargate to host the application container
-// const webService = new awsx.ecs.FargateService("web", {
-//     cluster: cluster.arn,
-//     taskDefinitionArgs: {
-//         container: {
-//             image: web.imageUri,
-//             cpu: cpu,
-//             memory: memory,
-//             essential: true,
-//             dependsOn: [],
-//             portMappings: [{
-//                 containerPort: containerPort,
-//                 targetGroup: loadbalancer.defaultTargetGroup,
-//             }],
-//         },
-//     },
-//     desiredCount: 3,
-//     tags: commonTags,
-// });
-
-const webTaskDefinition = new aws.ecs.TaskDefinition("web-task", {
-    family: "fargate-task-definition",
-    cpu: "256",
-    memory: "512",
-    networkMode: "awsvpc",
-    requiresCompatibilities: ["FARGATE"],
-    executionRoleArn: role.arn,
-    containerDefinitions: JSON.stringify([{
-      name: "web-app",
-      image: web.imageUri,
-      portMappings: [{
-        containerPort: 80,
-        hostPort: 80,
-        protocol: "tcp",
-      }],
-    }]),
+const webservice = new awsx.ecs.FargateService("web", {
+    cluster: cluster.arn,
+    taskDefinitionArgs: {
+        container: {
+            image: web.imageUri,
+            cpu: cpu,
+            memory: memory,
+            essential: true,
+            dependsOn: [],
+            portMappings: [{
+                containerPort: containerPort,
+                targetGroup: loadbalancer.defaultTargetGroup,
+            }],
+        },
+    },
+    desiredCount: 3,
     tags: commonTags,
-  });
-
-const webService = new aws.ecs.Service("web-svc", {
-  cluster: cluster.arn,
-  desiredCount: 3,
-  launchType: "FARGATE",
-  taskDefinition: webTaskDefinition.arn,
-  networkConfiguration: {
-    assignPublicIp: true,
-    subnets: vpc.privateSubnetIds,
-    securityGroups: [group.id],
-  },
-  loadBalancers: [{
-    targetGroupArn: targetGroup.arn,
-    containerName: "web-app",
-    containerPort: 80,
-  }],
-});  
-
-const apiTaskDefinition = new aws.ecs.TaskDefinition("api-task", {
-    family: "fargate-task-definition",
-    cpu: "256",
-    memory: "512",
-    networkMode: "awsvpc",
-    requiresCompatibilities: ["FARGATE"],
-    executionRoleArn: role.arn,
-    containerDefinitions: JSON.stringify([{
-      name: "api-app",
-      image: api.imageUri,
-      portMappings: [{
-        containerPort: 80,
-        hostPort: 80,
-        protocol: "tcp",
-      }],
-    }]),
-    tags: commonTags,
-  });
-
-const apiService = new aws.ecs.Service("api-svc", {
-  cluster: cluster.arn,
-  desiredCount: 3,
-  launchType: "FARGATE",
-  taskDefinition: apiTaskDefinition.arn,
-  networkConfiguration: {
-    subnets: vpc.privateSubnetIds,
-    securityGroups: [group.id],
-  },
-  loadBalancers: [{
-    targetGroupArn: targetGroup.arn,
-    containerName: "api-app",
-    containerPort: 80,
-  }],
 });
 
-// const apiService = new awsx.ecs.FargateService("api", {
-//     cluster: cluster.arn,
-//     assignPublicIp: false,
-//     taskDefinitionArgs: {
-//         container: {
-//             image: api.imageUri,
-//             cpu: cpu,
-//             memory: memory,
-//             essential: true,
-//             portMappings: [{
-//                 containerPort: containerPort,
-//             }],
-//         },
-//     },
-//     desiredCount: 3,
-//     tags: commonTags,
-// });
+const apiservice = new awsx.ecs.FargateService("api", {
+    cluster: cluster.arn,
+    assignPublicIp: false,
+    taskDefinitionArgs: {
+        container: {
+            image: api.imageUri,
+            cpu: cpu,
+            memory: memory,
+            essential: true,
+            portMappings: [{
+                containerPort: containerPort,
+            }],
+        },
+    },
+    desiredCount: 3,
+    tags: commonTags,
+});
 
 // The URL at which the container's HTTP endpoint will be available
-export const url = pulumi.interpolate`http://${loadbalancer.dnsName}`;
+export const url = pulumi.interpolate`http://${loadbalancer.loadBalancer.dnsName}`;
